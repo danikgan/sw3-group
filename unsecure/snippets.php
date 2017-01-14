36d7d32ae5a4c5a8b5716c749457b2ee5a775057
ZacVND <zac.luong.15@ucl.ac.uk>
2017-01-14 05:53:19 +0000

    add latest snippets to Home/start.php

--- unsecure/snippets.php
+++ unsecure/snippets.php
@@ -12,33 +12,33 @@
 <div id="logoarea">
     <img src="simpsons.png" alt="logo" />
 </div>
-		<table>
-			<tr><th>Name</th><th>Snippet</th></tr>
+	<table>
+		<tr><th>Name</th><th>Snippet</th></tr>
 
-			<?php
-			include_once('connect.php');
-			if(!isset($_SESSION))
-			{
-				session_start();
-			}
-			$name = $_SESSION["name"];
+		<?php
+		include_once('connect.php');
+		if(!isset($_SESSION))
+		{
+			session_start();
+		}
+		$name = $_SESSION["name"];
 
-			$rows = $conn->query('SELECT content, s.name as name, s.text_colour as text_colour FROM snippets JOIN students s ON snippets.fk_id = s.id');
+		$rows = $conn->query('SELECT content, s.name as name, s.text_colour as text_colour FROM snippets JOIN students s ON snippets.fk_id = s.id');
 
-			foreach ($rows as $row) {
-				$text_color = trim($row['text_colour']);
+		foreach ($rows as $row) {
+			$text_color = trim($row['text_colour']);
 
-				$snippet = trim($row['content']);
-				$student_name = trim($row['name']);
+			$snippet = trim($row['content']);
+			$student_name = trim($row['name']);
 
-				echo
-					 '<tr>
-								<td style="color: '.$text_color.'">'.$student_name.'</td>
-								<td style="color: '.$text_color.'">'.$snippet.'</td>
-					<tr>';
-			}
-			?>
-		</table>
+			echo
+				 '<tr>
+							<td style="color: '.$text_color.'">'.$student_name.'</td>
+							<td style="color: '.$text_color.'">'.$snippet.'</td>
+				<tr>';
+		}
+		?>
+	</table>
 
 </body>
 </html>
--- unsecure/start.php
+++ unsecure/start.php
@@ -16,11 +16,53 @@
 		<h1>Springfield Elementary Web Site</h1>
 
 		<ul>
-			<!-- Comment out the register page. It doesn't exist.
-			<li><a href="register.php">Sign Up for an Account</a></li>
-			-->
 			<li><a href="login.php">Log In</a></li>
 			<li><a href="register.php">Register</a></li>
 		</ul>
+<hr>
+		<table>
+			<tr><th>Name</th><th>Latest Snippet</th></tr>
+			<?php
+			include_once('connect.php');
+			if(!isset($_SESSION))
+			{
+				session_start();
+			}
+			$name = $_SESSION["name"];
+
+			$query = 'SELECT content, s.name as name, s.text_colour as text_colour
+						FROM   (
+						       SELECT (
+						              SELECT  id
+						              FROM    snippets s1
+						              WHERE   s1.fk_id = t1.fk_id
+						              ORDER BY
+						                      s1.id DESC
+						              LIMIT 1
+						              ) lid
+						       FROM   (
+						              SELECT  DISTINCT fk_id
+						              FROM    snippets
+						              ) t1
+						       ) ro, snippets t2
+						JOIN students s ON t2.fk_id = s.id
+						WHERE  t2.id = ro.lid';
+
+			$rows = $conn->query($query);
+
+			foreach ($rows as $row) {
+				$text_color = trim($row['text_colour']);
+
+				$snippet = trim($row['content']);
+				$student_name = trim($row['name']);
+
+				echo
+					 '<tr>
+								<td style="color: '.$text_color.'">'.$student_name.'</td>
+								<td style="color: '.$text_color.'">'.$snippet.'</td>
+					<tr>';
+			}
+			?>
+		</table>
 	</body>
 </html>
