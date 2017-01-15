ee03ff2178a03daf808ea64aa307133c35db0827
skulitom <artemskulimovskiy@gmail.com>
2017-01-15 19:19:54 +0000

    password removed

--- secure/connect.php
+++ secure/connect.php
@@ -1,8 +1,8 @@
 <?php
 include_once('sessionTimeout.php');
 $servername = "localhost";
-$username = "dev";
-$password = "0g14ju";
+$username = "root";
+$password = "";
 $dbname = "simpsons";
 
 // Create connection
